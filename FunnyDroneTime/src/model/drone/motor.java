0ee44cf2d658a29989d9f779d2bae682c795ce92
ryanzhang-200 <ryanzhang34567@gmail.com>
2025-10-15 20:55:21 -0700

    first commit

---
+++ FunnyDroneTime/src/model/drone/motor.java
@@ -0,0 +1,5 @@
+package model.drone;
+
+public class motor {
+    
+}
---
+++ FunnyDroneTime/src/ui/App.java
@@ -0,0 +1,9 @@
+package ui;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    //this is NOT an attempt to get hired to make razorblade
+    //pinky promise
+}
